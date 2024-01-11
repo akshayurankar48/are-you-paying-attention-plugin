d7f0cab2f02a2cbaacd557ea66b62ec2542e1fde
evilieswithin <evilieswithin>
2024-01-11 12:04:01 +0530

    first commit

---
+++ build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '805284399d8e8bd04393');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components'), 'version' => 'e04ab435113f2d2a0512');
